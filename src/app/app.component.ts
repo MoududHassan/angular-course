0d9fb9b1b5a80a86fe8f66e2cec39449a81368b3
Moudud <moudud@batworld.com>
2023-05-29 17:29:19 +0600

    ng content

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import {COURSES} from '../db-data';
 import { Course } from './model/course';
+import { CourseCardComponent } from './course-card/course-card.component';
 
 @Component({
   selector: 'app-root',
@@ -10,9 +11,37 @@ import { Course } from './model/course';
 export class AppComponent {
 
   courses = COURSES;
+  startDate = new Date();
+  price = 20.5;
+
+  // @ViewChild(CourseCardComponent)
+  // card:CourseCardComponent
+
+  @ViewChild('cardRef1')
+  card1:CourseCardComponent;
+
+  @ViewChild('cardRef2')
+  card2:CourseCardComponent;
+
+
+
+  // onCourseSelected(course:Course){
+  //   console.log("course card click, ", this.card1);
+  //   console.log("course card click, ", this.card2);
+  // }
+
+  // onCourseSelected(course:Course){
+  //   console.log("course card click, ", course);
+  // }
+
+  @ViewChild('container')
+  containerDiv:ElementRef;
+
+  @ViewChild('testContent')
+  testConyentDiv:ElementRef;
 
   onCourseSelected(course:Course){
-    console.log("course card click, ", course);
+    console.log("course container, ", this.testConyentDiv);
   }
 
 }
